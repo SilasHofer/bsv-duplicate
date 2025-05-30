beb8b792d7e5b637fdbd5bb26d0972d57e5baa2b
siho23 <siho23@student.bth.se>
2025-05-30 17:15:07 +0200

    test: detect_dublicates

--- test/unit/test_detect_duplicates.py
+++ test/unit/test_detect_duplicates.py
@@ -1,7 +1,79 @@
 import pytest
+from unittest.mock import patch 
+from src.util.detector import detect_duplicates
+
 
 # develop your test cases here
 
 @pytest.mark.unit
-def test_detect_duplicates():
-    assert True
+def test_detect_duplicates_only_one_artical():
+    data = [{"key": "12345",
+    "doi": "20020"}]
+    with patch("parser") as mock_pars:
+        mock_pars.return_value = data
+        with pytest.raises(ValueError):
+            detect_duplicates(data)
+
+@pytest.mark.unit
+def test_detect_duplicates_no_dublicate():
+    data = [{"key": "12345",
+    "doi": "20020"},{"key": "12343",
+    "doi": "20202"}]
+    expect = [""]
+    with patch("src.util.parser.parser") as mock_pars:
+        mock_pars.return_value = data
+        assert detect_duplicates(data) == expect
+
+
+@pytest.mark.unit
+def test_detect_duplicates_dublict_DOI_no_dublicat_key():
+    data = [{"key": "12345",
+    "doi": "20020"},{"key": "12343",
+    "doi": "20020"}]
+    expect = [{"key": "12345",
+    "doi": "20020"}]
+    with patch("parser") as mock_pars:
+        mock_pars.return_value = data
+        assert detect_duplicates(data) == expect
+
+@pytest.mark.unit
+def test_detect_duplicates_no_DOI_and_not_dublicat_key():
+    data = [{"key": "12345",
+    "doi": ""},{"key": "12343",
+    "doi": ""}]
+    expect = [""]
+    with patch("parser") as mock_pars:
+        mock_pars.return_value = data
+        assert detect_duplicates(data) == expect
+
+@pytest.mark.unit
+def test_detect_duplicates_one_no_DOI_and_not_dublicat_key():
+    data = [{"key": "12345",
+    "doi": ""},{"key": "12343",
+    "doi": "20202"}]
+    expect = [""]
+    with patch("parser") as mock_pars:
+        mock_pars.return_value = data
+        assert detect_duplicates(data) == expect
+
+@pytest.mark.unit
+def test_detect_duplicates_one_no_DOI_and_dublicat_key():
+    data = [{"key": "12345",
+    "doi": ""},{"key": "12345",
+    "doi": "20202"}]
+    expect = [{"key": "12345",
+    "doi": "20202"}]
+    with patch("parser") as mock_pars:
+        mock_pars.return_value = data
+        assert detect_duplicates(data) == expect
+
+@pytest.mark.unit
+def test_detect_duplicates_no_DOI_and_dublicat_key():
+    data = [{"key": "12345",
+    "doi": ""},{"key": "12345",
+    "doi": ""}]
+    expect = [{"key": "12345",
+    "doi": ""}]
+    with patch("parser") as mock_pars:
+        mock_pars.return_value = data
+        assert detect_duplicates(data) == expect
